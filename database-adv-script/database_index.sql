47c666b485b9dc046b5f0da17ceee2f848fea737
Enoch Nuertey <enochnuer111@gmail.com>
2025-05-18 21:07:19 +0000

    added explain analyze to measure query performance before and after indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,10 +1,16 @@
 -- CREATING INDEX
-
+EXPLAIN ANALYZE SELECT * FROM User WHERE user_id = '1';
 CREATE INDEX idx_user_id
 ON User (user_id)
+EXPLAIN ANALYZE SELECT * FROM User WHERE user_id = '1';
 
+
+EXPLAIN ANALYZE SELECT * FROM Property WHERE property_id = '1';
 CREATE INDEX idx_property_id
 ON Property (property_id)
+EXPLAIN ANALYZE SELECT * FROM Property WHERE property_id = '1';
 
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE booking_id = '1';
 CREATE INDEX idx_booking_id
 ON Booking (booking_id)
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE booking_id = '1';
